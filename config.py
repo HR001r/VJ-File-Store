cd8bf88cf518ef8f48922f5e5c2f1c289cd859cb
H.R <naruto.newgod@gmail.com>
2024-08-09 12:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,7 +60,7 @@ VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", ") # shortlink api
+SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/How_To_Open_Linkl") # how to open link 
 
 # Website Info:
